df350159954fd0575c5533b5084ce7fc585e6abd
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-04 01:06:30 -0500

    added href link for navbar and update

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 // create slow drag to each different section of the page
 // mouseover for each skill icon
 
-const navbarToggleButton = document.querySelector(".nav-toggle-icon")
+const navToggleButton = document.querySelector(".nav-toggle-icon")
 
-navbarToggleButton.addEventListener("click", ()=>{
-    console.log("yogurt");
+const navLinks = 
+
+navToggleButton.addEventListener("click", ()=>{
+    
 })
