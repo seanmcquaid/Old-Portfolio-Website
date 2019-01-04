d6650a9380619c9dc0044ce5fbb66ae1adbcbae6
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-03 16:21:27 -0800

    made placeholders for projects

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // create slow drag to each different section of the page
 // mouseover for each skill icon
 
-const navbarToggleButton = document.querySelector(".nav-toggle-button")
+const navbarToggleButton = document.querySelector(".nav-toggle-icon")
 
 navbarToggleButton.addEventListener("click", ()=>{
     console.log("yogurt");
