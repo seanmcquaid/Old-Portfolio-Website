4901ae0571177d0bc107b7545ae5b0c17e687b73
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-03 16:02:19 -0800

    made website responsive

--- scripts.js
+++ scripts.js
@@ -1,2 +1,8 @@
 // create slow drag to each different section of the page
 // mouseover for each skill icon
+
+const navbarToggleButton = document.querySelector(".nav-toggle-button")
+
+navbarToggleButton.addEventListener("click", ()=>{
+    console.log("yogurt");
+})
