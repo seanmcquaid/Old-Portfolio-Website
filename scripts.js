f33a958b3447d5b5a9e9bd12ac3eb05d1d7e1550
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-04 21:18:28 -0500

    making slow progress on mobile nav

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
-// create slow drag to each different section of the page
-// mouseover for each skill icon
-
+const backDrop = document.querySelector("#backdrop");
 const navToggleButton = document.querySelector(".nav-toggle-icon")
-
 const mobileNavLinks = document.querySelector(".mobile-nav")
 
 navToggleButton.addEventListener("click", ()=>{
     mobileNavLinks.style.display = "block";
-})
+    backdrop.style.display = 'block';
+});
+
+backDrop.addEventListener("click", ()=>{
+    mobileNavLinks.style.display = "none";
+    backdrop.style.display = 'none';
+});
